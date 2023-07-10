2c0aba011cd1c7b9dfccad2da296fab739a03096
Pablo Santana <pablosntojd@gmail.com>
2023-07-10 20:15:26 +0100

    feat: add third exercise initials.py

---
+++ learning-python/03-initials/initials.py
@@ -0,0 +1,9 @@
+# I've got 3 cats and 15 tattoos. Also have a project on Spotify called Kurayami.
+
+print('PPPP   SSS ')
+print('P   P S   S')
+print('P   P S    ')
+print('PPPP   SSS ')
+print('P         S')
+print('P     S   S')
+print('P      SSS ')
